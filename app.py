1ea504154bddec96d3bcd1fed50e10f8eceacfdd
Tim Ruffles <timruffles@googlemail.com>
2013-04-02 17:21:41 +0100

    add some logging

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -26,4 +28,8 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     if os.environ.get("DEBUG"):
       app.debug = True
+
+    stdout = logging.StreamHandler(sys.stdout)
+    app.logger.addHandler(stdout)
+
     app.run(host='0.0.0.0', port=port)
