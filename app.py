74675139a2b8b2bc60ea83567c8cb9ae11f4031e
Tim Ruffles <timruffles@googlemail.com>
2013-04-02 16:54:44 +0100

    hide venv

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  host = request.host.split(":")[1]
+  if host == "how-to-cancel-a-contract.co.uk":
+    return index_generic()
+  else:
+    return index_three()
+
+@app.route('/done')
+def done():
+  return render_template("done.html")
+
+def index_generic():
+  return render_template("generic.html",title="How to cancel a contract")
+
+def index_three():
+  return render_template("three.html",title="How to cancel a contract")
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
